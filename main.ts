8951595bc6501f502cf66731e920e28dd5b2f0fa
Peter Ogilvie <peter.ogilvie@coinbase.com>
2024-03-16 16:04:14 -0700

    add text-table

--- main.ts
+++ main.ts
@@ -1,20 +1,41 @@
 import { options, debug } from './options'
 import { getLimits } from './rest';
+import  table  from 'text-table';
 
 debug(options);
 
 const result = await getLimits();
 
-console.log(result['AnalyticsExternalDataSizeMB']);
+setInterval(pollLimits, 5000);
+
+type Limit = {
+    name: string;
+    max: number;
+    used: number;
+    remaining: number;
+};
+
+const limits: Limit[] = [];
+
 for (let stat in result) {
     let max = result[stat].Max;
     let remaining = result[stat].Remaining;
     let used = max - remaining;
 
     if (max != remaining) {
-        console.log(`${stat}: max: ${max} used: ${used}`);
+        // console.log(`${stat}: max: ${max} used: ${used}`);
+        limits.push({
+            name: stat as string,
+            max: max,
+            used: used,
+            remaining: remaining
+        });
     }
 }
 
 
-// console.log(result);
+console.log(limits);
+
+async function pollLimits() {
+    console.log('Hello, World!');
+}
