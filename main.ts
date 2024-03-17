fadc4a82f4f1351753b2e32b1c9105f3cfd25e50
Peter Ogilvie <peter.ogilvie@coinbase.com>
2024-03-17 11:34:08 -0700

    importing displayScreen breaks printTable

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
 import { options, debug } from './options'
 import { getLimits } from './rest';
 import { printTable } from './print-table';
-import { displayScreen } from './display-screen';
+// import { displayScreen } from './display-screen';
 
 debug(options);
 
 const result = await getLimits();
 
 if (!options.pollingTime) {
+    console.log('call print table');
     printTable(result);
     process.exit(0);
 } else {
-    displayScreen(options);
+    console.log('call display');
+    // displayScreen(options);
 }
 
 
