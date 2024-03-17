21b657d38fac508e4d80d92953f8a12fea093fb9
Peter Ogilvie <peter.ogilvie@coinbase.com>
2024-03-17 13:19:57 -0700

    simplify main

--- main.ts
+++ main.ts
@@ -5,14 +5,4 @@ import { printTable } from './print-table';
 debug(options);
 
 const result = await getLimits();
-
-if (!options.pollingTime) {
-    printTable(result);
-    process.exit(0);
-} else {
-    setInterval(pollLimits, options.pollingTime * 1000);
-}
-
-async function pollLimits() {
-    console.log('Hello, World!');
-}
+printTable(result);
